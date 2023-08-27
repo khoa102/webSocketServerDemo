6364bdc6c33f59c7722ddb15ad7735a98983c106
Khoa Tran <khoa102@gmail.com>
2023-08-27 10:54:04 -0700

    Initializing git project

---
+++ src/main/java/com/khoatran/websocketServer/WebsocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.khoatran.websocketServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketServerApplication.class, args);
+	}
+
+}
